c9a774420da949518be54f19108319e302946594
Shanqing Cai <cais@google.com>
2022-05-04 10:26:38 -0400

    Prevent referencing GazeDevice for now

--- app/WebWatcher/WebWatcher/HostInfo.cs
+++ app/WebWatcher/WebWatcher/HostInfo.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Toolkit.Uwp.Input.GazeInteraction.Device;
+﻿//using Microsoft.Toolkit.Uwp.Input.GazeInteraction.Device;
 
 namespace WebWatcher
 {
@@ -7,16 +7,17 @@ namespace WebWatcher
         // Version string for the host app.
         private static string VERSION_MAJOR = "0";
         private static string VERSION_MINOR = "0";
-        private static string VERSION_PATCH = "3";
+        private static string VERSION_PATCH = "4";
         private static string VERSION =
             $"{VERSION_MAJOR}.{VERSION_MINOR}.{VERSION_PATCH}";
         public static string GetSerializedHostInfo()
         {
             string tobiiStreamEngineVersion = "";
-            if (GazeDevice.Instance != null)
-            {
-                tobiiStreamEngineVersion = GazeDevice.Instance.GetVersion();
-            }
+            // TODO(cais): Add reference. Prevent leak and failure to quit.
+            //if (GazeDevice.Instance != null)
+            //{
+                //tobiiStreamEngineVersion = GazeDevice.Instance.GetVersion();
+            //}
             string hostInfoString =
                 $"{{\"hostAppVersion\": \"{VERSION}\", \"engineVersion\": \"{tobiiStreamEngineVersion}\"}}";
             return hostInfoString;
